d3bd7d9a922c0d4c85e8db5a71736c876dc00cfe
johny077sk <marcincakjan@gmail.com>
2016-07-06 13:35:06 +0200

    Update follows.js

--- follows.js
+++ follows.js
@@ -146,7 +146,7 @@
 						 			// Build Graph
 									var graph = new Rickshaw.Graph( {
 										element: document.querySelector('#graph-' + feedId + '-' + datastream.id),
-										width: 800,
+										width: 1000,
 										height: 400,
 										renderer: 'line',
 										min: parseFloat(datastream.min_value) - .25*(parseFloat(datastream.max_value) - parseFloat(datastream.min_value)),
