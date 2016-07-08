0d09c61e36ff08220b6b4b1b89929a3fd74b2286
johny077sk <marcincakjan@gmail.com>
2016-07-08 14:08:58 +0200

    Update follows.js

--- follows.js
+++ follows.js
@@ -162,7 +162,7 @@
 				// Build Graph
 									var palette = new Rickshaw.Color.Palette( { scheme: 'classic9' } );
 									var graph = new Rickshaw.Graph( {
-										element: document.querySelector('#graph-' + feedId + '-' ),//+ datastream.id),
+										element: document.querySelector('#graph-' + feedId + '-' + datastream.id),
 										width: 900,
 										height: 300,
 										renderer: 'line',
